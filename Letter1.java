95698719f2845c6462981236e8ba074ca9b16661
unknown <Kushalwadatkar>
2021-12-28 14:03:10 +0530

    commit

---
+++ Letter1.java
@@ -0,0 +1,23 @@
+package com.letter;
+
+public class Letter1 {
+
+	public static void main(String[] args) {
+
+		char value = 'v';
+		switch (value) {
+		case 'u':
+		case 'm':
+		case 'b':
+		case 'r':
+		case 'e':
+		case 'l':
+		case 'a':
+			System.out.println(" Is present in Umbrella");
+			break;
+		default:
+			System.out.println("Is not present in Umbrella");
+		}
+
+	}
+}
